dfda671f07b0286f40d338261abe21e8c36f202b
Xaiodo <rostislavgovbak@gmail.com>
2023-08-08 18:15:17 +0300

    Added initial theme settings

---
+++ lib/config/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/config/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.backgroundLightColor,
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(
+        color: AppColors.primaryLightColor,
+      ),
+      backgroundColor: AppColors.appBarBackgroundLightColor,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      // TODO: Add text styles
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.backgroundDarkColor,
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(
+        color: AppColors.primaryDarkColor,
+      ),
+      backgroundColor: AppColors.containerDarkColor,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      // TODO: Add text styles
+    ),
+  );
+}
