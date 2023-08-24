74a95ba2feb660a69b0fda7c78512daddaf11379
Xaiodo <rostislavgovbak@gmail.com>
2023-08-24 18:11:56 +0300

    Created hourly weather forecast model

---
+++ lib/src/weather_forecast/data/models/weather_hourly.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_hourly.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_WeatherHourlyModel _$$_WeatherHourlyModelFromJson(
+        Map<String, dynamic> json) =>
+    _$_WeatherHourlyModel(
+      condition: json['condition'] as String,
+      icon: $enumDecode(_$WeatherIconEnumMap, json['icon']),
+      datetime: DateTime.parse(json['datetime'] as String),
+      temperature: (json['temp'] as num).toDouble(),
+      tempMin: (json['tempmin'] as num).toDouble(),
+      tempMax: (json['tempmax'] as num).toDouble(),
+      feelsLike: (json['feelslike'] as num).toDouble(),
+      humidity: (json['humidity'] as num).toDouble(),
+      windSpeed: (json['windspeed'] as num).toDouble(),
+      pressure: (json['pressure'] as num).toDouble(),
+      uvIndex: (json['uvindex'] as num).toDouble(),
+      snow: (json['snow'] as num).toDouble(),
+      snowDepth: (json['snowdepth'] as num).toDouble(),
+      precipitationType: (json['preciptype'] as List<dynamic>)
+          .map((e) => $enumDecode(_$WeatherPrecipitationTypeEnumMap, e))
+          .toList(),
+      precipitationProbability: (json['precipprob'] as num).toDouble(),
+      precipitation: (json['precip'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$$_WeatherHourlyModelToJson(
+        _$_WeatherHourlyModel instance) =>
+    <String, dynamic>{
+      'condition': instance.condition,
+      'icon': _$WeatherIconEnumMap[instance.icon]!,
+      'datetime': instance.datetime.toIso8601String(),
+      'temp': instance.temperature,
+      'tempmin': instance.tempMin,
+      'tempmax': instance.tempMax,
+      'feelslike': instance.feelsLike,
+      'humidity': instance.humidity,
+      'windspeed': instance.windSpeed,
+      'pressure': instance.pressure,
+      'uvindex': instance.uvIndex,
+      'snow': instance.snow,
+      'snowdepth': instance.snowDepth,
+      'preciptype': instance.precipitationType
+          .map((e) => _$WeatherPrecipitationTypeEnumMap[e]!)
+          .toList(),
+      'precipprob': instance.precipitationProbability,
+      'precip': instance.precipitation,
+    };
+
+const _$WeatherIconEnumMap = {
+  WeatherIcon.clearDay: 'clearDay',
+  WeatherIcon.clearNight: 'clearNight',
+  WeatherIcon.cloudy: 'cloudy',
+  WeatherIcon.fog: 'fog',
+  WeatherIcon.hail: 'hail',
+  WeatherIcon.partlyCloudyDay: 'partlyCloudyDay',
+  WeatherIcon.partlyCloudyNight: 'partlyCloudyNight',
+  WeatherIcon.rain: 'rain',
+  WeatherIcon.sleet: 'sleet',
+  WeatherIcon.snow: 'snow',
+  WeatherIcon.thunderstorm: 'thunderstorm',
+  WeatherIcon.tornado: 'tornado',
+  WeatherIcon.wind: 'wind',
+  WeatherIcon.unknown: 'unknown',
+};
+
+const _$WeatherPrecipitationTypeEnumMap = {
+  WeatherPrecipitationType.rain: 'rain',
+  WeatherPrecipitationType.snow: 'snow',
+  WeatherPrecipitationType.freezingRain: 'freezingRain',
+  WeatherPrecipitationType.ice: 'ice',
+  WeatherPrecipitationType.unknown: 'unknown',
+};
