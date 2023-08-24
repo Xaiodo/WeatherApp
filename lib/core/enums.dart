9a1948466029ba5f3c4f17e15d1e636d73a7d0ba
Xaiodo <rostislavgovbak@gmail.com>
2023-08-24 17:38:56 +0300

    Created weather icons and precipitation type enums

---
+++ lib/core/enums.dart
@@ -0,0 +1,24 @@
+enum WeatherIcon {
+  clearDay,
+  clearNight,
+  cloudy,
+  fog,
+  hail,
+  partlyCloudyDay,
+  partlyCloudyNight,
+  rain,
+  sleet,
+  snow,
+  thunderstorm,
+  tornado,
+  wind,
+  unknown,
+}
+
+enum WeatherPrecipitationType {
+  rain,
+  snow,
+  freezingRain,
+  ice,
+  unknown,
+}
